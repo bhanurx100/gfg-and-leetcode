109252bef11ab00ef89203edb6f7620cbaab9363
Bhanuprasad L <bhanu21r@gmail.com>
2024-12-23 23:58:35 +0530

    Time: 54 ms (47.68%), Space: 18.5 MB (17.96%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.py
+++ 0838-design-linked-list/0838-design-linked-list.py
@@ -1,27 +1,104 @@
-class MyLinkedList:
+class Node(object):
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class MyLinkedList(object):
 
     def __init__(self):
-        self.arr = []
+        """
+        Initialize your data structure here.
+        """
+        self.head = None
+        self.size = 0
 
-    def get(self, index: int) -> int:
-        # O(1)
-        return self.arr[index] if index < len(self.arr) and index >= 0 else -1
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        if index < 0 or index >= self.size:
+            return -1
 
-    def addAtHead(self, val: int) -> None:
-        # O(n)
-        self.arr.insert(0, val)
+        if self.head is None:
+            return -1
 
-    def addAtTail(self, val: int) -> None:
-        # O(1)
-        self.arr.append(val)
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        return curr.val
 
-    def addAtIndex(self, index: int, val: int) -> None:
-        # O(n)
-        if index == len(self.arr):
-            self.addAtTail(val)
-        elif index < len(self.arr) and index >= 0:
-            self.arr.insert(index, val)
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list.
+        After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        node = Node(val)
+        node.next = self.head
+        self.head = node
 
-    def deleteAtIndex(self, index: int) -> None:
-        if index < len(self.arr) and index >= 0:
-            self.arr.pop(index)
+        self.size += 1
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        curr = self.head
+        if curr is None:
+            self.head = Node(val)
+        else:
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = Node(val)
+
+        self.size += 1
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list.
+        If index equals to the length of linked list, the node will be appended to the end of linked list.
+        If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: void
+        """
+        if index < 0 or index > self.size:
+            return
+
+        if index == 0:
+            self.addAtHead(val)
+        else:
+            curr = self.head
+            for i in range(index - 1):
+                curr = curr.next
+            node = Node(val)
+            node.next = curr.next
+            curr.next = node
+
+            self.size += 1
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: void
+        """
+        if index < 0 or index >= self.size:
+            return
+
+        curr = self.head
+        if index == 0:
+            self.head = curr.next
+        else:
+            for i in range(index - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+
+        self.size -= 1
