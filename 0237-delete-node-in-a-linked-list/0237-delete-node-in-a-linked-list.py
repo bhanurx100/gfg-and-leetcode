7852f12b3ac35e172cf117cd4b7bf3be917c0dc4
Bhanuprasad L <bhanu21r@gmail.com>
2024-12-23 22:20:56 +0530

    Time: 36 ms (74.77%), Space: 18.3 MB (6.15%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -7,9 +7,5 @@
 class Solution:
     # Function to delete a node from a singly linked list
     def deleteNode(self, node: ListNode) -> None:
-        # Store the next node's pointer temporarily
-        nextNode = node.next.next
-        # Copy the value of the next node to the current node
         node.val = node.next.val
-        # Update the next pointer to skip the next node
-        node.next = nextNode
+        node.next = node.next.next
