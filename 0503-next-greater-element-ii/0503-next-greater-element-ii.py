4a28af7fb5ea73be32bef0459866eb30a7604ac4
Bhanuprasad L <bhanu21r@gmail.com>
2024-12-29 18:47:33 +0530

    Time: 17 ms (84.74%), Space: 19.7 MB (7.47%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,20 +1,13 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        # Initialize a stack to keep track of indices of elements we haven't found a next greater element for
-        stack = []
-        
-        # Initialize the result list to contain all -1s (assuming we won't find any next greater element)
-        result = [-1] * len(nums)
-        
-        # Loop through the array twice (to handle circularity) and process each element
-        for _ in range(2):
-            for i in range(len(nums)):
-                # Pop elements off the stack if they are less than the current element and update their result
-                while stack and nums[stack[-1]] < nums[i]:
-                    result[stack.pop()] = nums[i]
-                
-                # Add the current index to the stack
-                stack.append(i)
-        
-        # Return the result list
-        return result
+        n=len(nums)
+        st=[]
+        res=[-1]*n
+        for i in range (2*n):
+            current_index=i%n
+            while st and nums[current_index]>nums[st[-1]]:
+                top_index=st.pop()
+                res[top_index]=nums[current_index]
+            if i<n:
+                st.append(current_index)
+        return res
