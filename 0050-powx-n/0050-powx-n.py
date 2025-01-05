59139c189ef029217e85570347c0635f529f5a33
Bhanuprasad L <bhanu21r@gmail.com>
2025-01-05 22:19:59 +0530

    Time: 0 ms (100%), Space: 18.1 MB (7.42%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,17 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        # Handle the case where n is negative
-        if n < 0:
-            x = 1 / x
-            n = -n
-        
-        def fast_pow(base, exp):
-            if exp == 0:
-                return 1
-            half = fast_pow(base, exp // 2)
-            if exp % 2 == 0:
-                return half * half
-            else:
-                return half * half * base
-        
-        return fast_pow(x, n)
+        if n<0:
+            x=1/x
+            n=-n
+        res=1
+        curr_product=x
+        while n>0:
+            if n%2==1:
+                res*=curr_product
+            curr_product*=curr_product
+            n//=2
+        return res
