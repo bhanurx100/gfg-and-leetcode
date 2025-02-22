c68aeac517f310749f021f8fdbaa9c1869662a23
Bhanuprasad L <bhanu21r@gmail.com>
2025-02-22 23:59:55 +0530

    Time: 43 ms (61.11%), Space: 32.7 MB (64.08%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
@@ -1,26 +1,43 @@
+from typing import List
+
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        parent = list(range(n))
-        self.count = n
-        self.redundant = 0
-        def find(x):
-            if x != parent[x]:
-                parent[x] = find(parent[x])
-            return parent[x]
-        def union(x,y):
-            rootx = find(x)
-            rooty = find(y)
-            if rootx != rooty:
-                parent[rootx] = rooty
-                self.count -= 1
-            else:
-                self.redundant +=1
-        
-        for x, y in connections:
-            union(x, y)
-        
-        if self.redundant >= self.count - 1:
-            return self.count - 1
-        else:
+        # Step 1: If there aren't enough edges to connect all nodes, return -1
+        if len(connections) < n - 1:
             return -1
-			
+        
+        # Step 2: Union-Find (Disjoint Set Union - DSU)
+        parent = [i for i in range(n)]  # Each node is its own parent
+        rank = [1] * n  # Rank for Union by Rank
+
+        # Find with Path Compression
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])  # Path compression
+            return parent[x]
+
+        # Union by Rank
+        def union(x, y):
+            rootX = find(x)
+            rootY = find(y)
+            
+            if rootX != rootY:  # Merge if different components
+                if rank[rootX] > rank[rootY]:
+                    parent[rootY] = rootX
+                elif rank[rootX] < rank[rootY]:
+                    parent[rootX] = rootY
+                else:
+                    parent[rootY] = rootX
+                    rank[rootX] += 1
+                return True  # Successful merge
+            return False  # Already connected
+        
+        # Step 3: Apply Union-Find on given connections
+        for u, v in connections:
+            union(u, v)
+        
+        # Step 4: Count number of connected components
+        components = len(set(find(i) for i in range(n)))
+
+        # Step 5: Minimum operations required = (components - 1)
+        return components - 1
