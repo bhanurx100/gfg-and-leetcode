c7f42f324612f2de273722229a3fb2dd295b4a55
Bhanuprasad L <bhanu21r@gmail.com>
2025-03-11 21:58:24 +0530

    Time: 75 ms (83.67%), Space: 17.9 MB (67.33%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,12 +1,12 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        char_count = {'a': 0, 'b': 0, 'c': 0}
-        left = 0
+        dp = [None] * 3
         result = 0
-        for right in range(len(s)):
-            char_count[s[right]] += 1
-            while char_count['a'] > 0 and char_count['b'] > 0 and char_count['c'] > 0:
-                result += len(s) - right
-                char_count[s[left]] -= 1
-                left += 1
+        for i in range(len(s)):
+            # ord(s[i]) convert to ASCII code, a,b,c = 97,98,99 
+            # -> ord(s[i] - 97) means that a,b,c = 0,1,2
+            # Additionally, I store i+1 instead of +1 when calculating and conveniently using all() function.
+            dp[ord(s[i])-97] = i+1
+            if all(dp):
+                result += min(dp)
         return result
