ca1814e9ade751133bf4da1d12e7e516e9a3c2a1
Bhanuprasad L <bhanu21r@gmail.com>
2025-08-31 22:29:54 +0530

    Time: 1079 ms (75.67%), Space: 18 MB (63.32%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,38 +1,51 @@
+from typing import List
+
 class Solution:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows=[set() for _ in range(9)]
-        cols=[set() for _ in range(9)]
-        boxes=[set() for _ in range(9)]
+
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empties = []
+
+        # Initialize sets with existing numbers
         for r in range(9):
             for c in range(9):
-                if board[r][c]!='.':
-                    val=board[r][c]
-                    rows[r].add(val)
-                    cols[c].add(val)
-                    box_index=(r//3)*3+(c//3)
-                    boxes[box_index].add(val)
-        def backtrack(r,c):
-            if  c==9:
-                return backtrack(r+1,0)
-            if r==9:
-                return True
-            if board[r][c]!='.':
-                return backtrack(r,c+1)
-            for num in map(str,range(1,10)):
-                box_index=(r//3)*3+(c//3)
-                if num not in rows[r] and num not in cols[c] and num not in boxes[box_index]:
-                    board[r][c]=num
+                if board[r][c] == '.':
+                    empties.append((r, c))
+                else:
+                    num = board[r][c]
                     rows[r].add(num)
                     cols[c].add(num)
-                    boxes[box_index].add(num)
-                    if backtrack(r,c+1):
+                    boxes[(r//3)*3 + (c//3)].add(num)
+
+        def dfs(idx=0):
+            if idx == len(empties):   # filled all empty cells
+                return True
+
+            r, c = empties[idx]
+            b = (r//3)*3 + (c//3)
+
+            for num in '123456789':
+                if num not in rows[r] and num not in cols[c] and num not in boxes[b]:
+                    # Place num
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[b].add(num)
+
+                    if dfs(idx+1):
                         return True
-                    board[r][c]='.'
+
+                    # Backtrack
+                    board[r][c] = '.'
                     rows[r].remove(num)
                     cols[c].remove(num)
-                    boxes[box_index].remove(num)
+                    boxes[b].remove(num)
+
             return False
-        backtrack(0,0)
+
+        dfs()
